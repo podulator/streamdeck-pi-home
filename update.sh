050df52485eb54c224d80c32de94e03aab5488e9
podulator <mat@podulator.com>
2024-02-11 17:43:35 +0100

    update.sh now accurately checks for local unignored additions or modified files

--- update.sh
+++ update.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/bash
 
-modified=$(git status -s | wc -l)
+added=$(git status -s -u normal | wc -l)
+modified=$(git ls-files -m | wc -l)
 if [ ${modified} -ne 0 ]; then
-	echo "Resolve local modifications"
+	echo "Local modifications : "
+	git ls-files -m | head -n 2
+	exit 1
+elif [ ${added} -ne 0 ]; then
+	echo "Locally added files : "
 	git status -s | head -n 2
 	exit 1
 else
 	# silent upgrade of pip
-	pip install -Ur ./requirements.txt 1>/dev/null 2>&1
+	./venv/bin/activate && pip install -Ur ./requirements.txt 1>/dev/null 2>&1
 	git pull | tail -n 1
 fi
